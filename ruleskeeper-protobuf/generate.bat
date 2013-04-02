960d3a8b2c1b1d149ce3e7a82377b19b69345219
RulesKeeper <contact@ruleskeeper.org>
2013-04-02 22:10:09 +0200

    Add missing install.bat and generate.bat to generate ProtoBuf JAVA files

---
+++ ruleskeeper-protobuf/generate.bat
@@ -0,0 +1,1 @@
+mvn -f pom-compile.xml compile
--- ruleskeeper-protobuf/install.bat
+++ ruleskeeper-protobuf/install.bat
@@ -1,4 +1,6 @@
 @echo off 
 
-set MAVEN_OPTS="-Xmx1024m" 
-call mvn eclipse:clean eclipse:eclipse clean package install -DskipTests -DdownloadSources=false -DdownloadDocs=false
+set MAVEN_OPTS="-Xmx1024m"
+call mvn eclipse:clean clean
+call generate.bat 
+call mvn eclipse:eclipse package install -DskipTests -DdownloadSources=false -DdownloadDocs=false
